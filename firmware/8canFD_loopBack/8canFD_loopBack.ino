e0c7f77ffdea4a191997e62fadfaa97246c42e99
Thomas FLayols <thomas.flayols@gmail.com>
2019-02-07 19:06:00 +0100

    extend to 8 CAN

--- firmware/8canFD_loopBack/8canFD_loopBack.ino
+++ firmware/8canFD_loopBack/8canFD_loopBack.ino
@@ -1,15 +1,40 @@
 #include <ACAN2517.h>
 
-
-static const byte CS_CAN_A = PA3; 
-static const byte CS_CAN_B = PA4; 
-
+#define CS_CAN_A  PB11
+#define CS_CAN_B  PB10 
+#define CS_CAN_C  PB1 
+#define CS_CAN_D  PB0 
+#define CS_CAN_E  PA4 
+#define CS_CAN_F  PA3 
+#define CS_CAN_G  PA2 
+#define CS_CAN_H  PA1
+                   
 ACAN2517 can_A(CS_CAN_A, SPI, 255); 
 ACAN2517 can_B(CS_CAN_B, SPI, 255); 
+ACAN2517 can_C(CS_CAN_C, SPI, 255);
+ACAN2517 can_D(CS_CAN_D, SPI, 255);
+ACAN2517 can_E(CS_CAN_E, SPI, 255);
+ACAN2517 can_F(CS_CAN_F, SPI, 255);
+ACAN2517 can_G(CS_CAN_G, SPI, 255);
+ACAN2517 can_H(CS_CAN_H, SPI, 255);
 
+#define NUMBER_OF_CAN 8
+
+ACAN2517 *can[NUMBER_OF_CAN];
+
+int i;
 void setup()
 {
-  //--- Switch on builtin led
+  can[0] = &can_A;
+  can[1] = &can_B;
+  can[2] = &can_C;
+  can[3] = &can_D;
+  can[4] = &can_E;
+  can[5] = &can_F;
+  can[6] = &can_G;
+  can[7] = &can_H;
+  
+    //--- Switch on builtin led
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   
@@ -18,81 +43,58 @@ void setup()
   delay(100);
   SPI.begin();
   
-  ACAN2517Settings settings_A(ACAN2517Settings::OSC_20MHz, 125 * 1000); // CAN bit rate 125 kb/s
-  ACAN2517Settings settings_B(ACAN2517Settings::OSC_20MHz, 125 * 1000); // CAN bit rate 125 kb/s
-  
-  settings_A.mRequestedMode = ACAN2517Settings::InternalLoopBack;       // Select loopback mode
-  settings_B.mRequestedMode = ACAN2517Settings::InternalLoopBack;       // Select loopback mode
-  
-  const uint32_t errorCode_A = can_A.begin(settings_A, NULL);               // No ISR
-  Serial.print ("Configuration error code A 0x") ;
-  Serial.println (errorCode_A, HEX) ;
-  can_A.writeByteRegister (0xE00, 0x00);                                //to get 20Mhz as output clock TODO add it in the lib
-  
-  delay(10);
-  const uint32_t errorCode_B = can_B.begin(settings_B, NULL);               // No ISR  
-  Serial.print ("Configuration error code B 0x") ;
-  Serial.println (errorCode_B, HEX) ; 
-  can_B.writeByteRegister (0xE00, 0x00);                                //to get 20Mhz as output clock TODO add it in the lib
-  
+  ACAN2517Settings settings(ACAN2517Settings::OSC_20MHz, 125 * 1000); // CAN bit rate 125 kb/s
+  settings.mRequestedMode = ACAN2517Settings::InternalLoopBack;       // Select loopback mode
+  uint32_t errorCode[NUMBER_OF_CAN]={0};
+  for (i = 0;i<NUMBER_OF_CAN;i++)
+  {
+      errorCode[i] = can[i]->begin(settings, NULL);               // No ISR
+      Serial.print ("Configuration error code 0x") ;
+      Serial.println (errorCode[i], HEX) ;
+      can[i]->writeByteRegister (0xE00, 0x00);  //to get 20Mhz as output clock TODO add it in the lib
+  }
 }
 
 static unsigned gBlinkLedDate = 0;
-static unsigned gReceivedFrameCount_A=0;
-static unsigned gReceivedFrameCount_B=0;
-
-static unsigned gSentFrameCount_A=0;
-static unsigned gSentFrameCount_B=0;
+static unsigned gReceivedFrameCount[NUMBER_OF_CAN]={0};
+static unsigned gSentFrameCount[NUMBER_OF_CAN]={0};
 
 void loop()
 {
-  can_A.poll(); // Call can.poll as often as possible
-  //delay(1);
-  can_B.poll(); // Call can.poll as often as possible
+  for (i = 0;i<NUMBER_OF_CAN;i++)
+  {
+    can[i]->poll(); // Call can.poll as often as possible
+  }
   CANMessage frame;
   if (gBlinkLedDate < millis())
   {
     gBlinkLedDate += 2000;
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    const bool ok_A = can_A.tryToSend(frame);
-    const bool ok_B = can_B.tryToSend(frame);
-    if (ok_A)
+    for (i = 0;i<NUMBER_OF_CAN;i++)
     {
-      gSentFrameCount_A += 1;
-      Serial.print("CAN A Sent: ");
-      Serial.println(gSentFrameCount_A);
+      if(can[i]->tryToSend(frame))
+      {
+      gSentFrameCount[i]++;
+      Serial.print("CAN Sent: ");
+      Serial.println(gSentFrameCount[i]);
+      }
+      else 
+      {
+        Serial.println("CAN A Send failure");
+      }
     }
-    else
-    {
-      Serial.println("CAN A Send failure");
-    }
-    if (ok_B)
-    {
-      gSentFrameCount_B += 1;
-      Serial.print("CAN B Sent: ");
-      Serial.println(gSentFrameCount_B);
-    }
-    else
-    {
-      Serial.println("CAN B Send failure");
-    }
-
-    
   }
-  if (can_A.available())
+  for (i = 0;i<NUMBER_OF_CAN;i++)
   {
-    can_A.receive(frame);
-    gReceivedFrameCount_A++;
-    Serial.print("CAN_A Received: ");
-    Serial.println(gReceivedFrameCount_A);
+    if (can[i]->available())
+    {
+      can[i]->receive(frame);
+      gReceivedFrameCount[i]++;
+      Serial.print("CAN Received: ");
+      Serial.println(gReceivedFrameCount[i]);
+    }
   }
-  if (can_B.available())
-  {
-    can_B.receive(frame);
-    gReceivedFrameCount_B++;
-    Serial.print("CAN_B Received: ");
-    Serial.println(gReceivedFrameCount_B);
-  }
+ 
 
   
 }
